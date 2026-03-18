596d99ac85758c5e7a2f9b66b5769afc409f3a02
Ivan-GB <ivangomezba07@gmail.com>
2026-03-17 19:56:56 -0500

    Add MyQueue and MyStack
    Se agregan las clases MyQueue y My Stack

---
+++ MyQueue.java
@@ -0,0 +1,61 @@
+package org.example;
+
+public class MyQueue<T> {
+    T[] Stack;
+    int  size=0;
+    int capacity=1;
+    int top=0;
+    public MyQueue(){
+        Stack=(T[])new Object[1];
+    }
+    public void enqueue(T o){
+        resize();
+        Stack[(top+size)%capacity]=o;
+        size++;
+    }
+    public void resize(){
+        if(size==Stack.length){
+            capacity*=2;
+            T[] NStack=(T[])new Object[capacity];
+            for(int i=0;i<size;i++){
+                NStack[i]=Stack[(top+i)%capacity];
+            }
+            Stack=NStack;
+        }
+    }
+    public T dequeue(){
+        if(size==0){
+            return null;
+        }
+        T o=Stack[top];
+        for(int i=0;i<size-1;i++){
+            Stack[(top+i)%capacity]=Stack[((top+i+1)%capacity)];
+        }
+        Stack[(top+size-1)%capacity]=null;
+        size--;
+        return o;
+    }
+    public T peek(){
+        if(size==0){
+            return null;
+        }
+        return Stack[top];
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public int size(){
+        return size;
+    }
+    public void delete(T o){
+        for(int i=0;i<size;i++){
+            if(o==Stack[(top+i)%capacity]){
+                for(int j=i;j<size-1;j++){
+                    Stack[(top+j)%capacity]=Stack[(top+j+1)%capacity];
+                }
+                size--;
+                break;
+            }
+        }
+    }
+}
