e7a9791cf1a5acdf36eaaa91c1275363fe9a5c90
Andrés Tellez <90323239+ASTG373@users.noreply.github.com>
2026-03-21 10:51:26 -0500

    Change find method visibility from private to public

--- DoublyLinked.java
+++ DoublyLinked.java
@@ -82,7 +82,7 @@ public class DoublyLinked<T> {
         return tail.value;
     }
     
-    private Node<T> find(T val) {
+    public Node<T> find(T val) {
         Node<T> n = head;
         
         while (n != null) {
