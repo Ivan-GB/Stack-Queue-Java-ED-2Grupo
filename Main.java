dc4dab6db7adde708a795891d9acfff5eb849e5f
Jesus Gaitan <166076219+JesusG16@users.noreply.github.com>
2026-03-22 21:45:24 -0500

    WIP generación de resultados, gráficas y estadisticas

--- Main.java
+++ Main.java
@@ -19,17 +19,24 @@ public class Main{
         return new String[]{DS, op, String.valueOf(size), String.valueOf(nanoTime)};
     }
 
-    private static void testLinkedList (LinkedLists L, int[] data, int index, int dato, List<String[]> rows, int size, String name){
+    private static void testLinkedList (LinkedLists L, List<String[]> rows, int size, String name){
+        DataGenerator generator = new DataGenerator(0, size-1, 123123); //Min, max, seed
+        //Esto generará un número random para el index del addBefore y el addAfter
+        int index = generator.next();
+        int dato = generator.next();
+        
         //Llenamos full la list para hacer las mediciones en estructuras llenas:
-        for(int i=0; i<size; i++) L.pushFront(data[i]);
-       
+        for(int i=0; i<size/2; i++) L.pushFront(generator.next());
+        
         long t = measure(()->{
-            L.pushFront(data[0]);
+            L.pushFront(index);
         });
         rows.add(row(name, "PushFront", size, t));
         
+        for(int i=size/2; i<size; i++) L.pushFront(generator.next());
+
         t = measure(()->{
-            L.pushBack(data[size-1]);
+            L.pushBack(generator.next());
         });
         rows.add(row(name, "PushBack", size, t));
         
@@ -39,23 +46,23 @@ public class Main{
         rows.add(row(name, "isEmpty", size, t));
         
         t = measure(()->{
-            L.addAfter(data[index], dato);
+            L.addAfter(index, dato);
         });
         rows.add(row(name, "AddAfter", size, t));
         
         t = measure(()->{
-            L.addBefore(data[index], dato);
+            L.addBefore(index, dato);
         });
         rows.add(row(name, "AddBefore", size, t));
         
         t = measure(()->{
-            L.find(data[index]);
+            L.find(index);
         });
         rows.add(row(name, "Find", size, t));
         
         t = measure(()->{
             //Pasamos un elemento que sí o sí debe estar
-            L.erase(data[index]); 
+            L.erase(index); 
         });
         rows.add(row(name, "Erase", size, t));
         
@@ -70,9 +77,91 @@ public class Main{
         rows.add(row(name, "PopBack", size, t));
     }
 
+    private static void testQueue (MyQueue mq, List<String[]> rows, int size){
+        DataGenerator generator = new DataGenerator(0, size-1, 123123); //Min, max, seed
+        int dato = generator.next();
+        
+        //Llenamos la queue:
+        for(int i=1; i<size/2; i++) mq.enqueue(generator.next());
+
+        long t = measure(()->{
+            mq.enqueue(dato);
+        });
+        rows.add(row("Queue", "Enqueue", size, t));
+        
+        for(int i=size/2; i<size; i++) mq.enqueue(generator.next());
+
+        t = measure(()->{
+            bh.consume(mq.dequeue());
+        });
+        rows.add(row("Queue", "Dequeue", size, t));
+        
+        t = measure(()->{
+            mq.delete(dato);
+        });
+        rows.add(row("Queue", "Delete", size, t));
+        
+        //Lo dejo como front porque así aparece en el doc guía
+        t = measure(()->{
+            bh.consume(mq.peek());
+        });
+        rows.add(row("Queue", "Front", size, t));
+        
+        t = measure(()->{
+            bh.consume(mq.size());
+        });
+        rows.add(row("Queue", "Size", size, t));
+        
+        t = measure(()->{
+            bh.consume(mq.isEmpty());
+        });
+        rows.add(row("Queue", "isEmpty", size, t));
+    }
+
+    private static void testStack (MyStack ms, List<String[]> rows, int size){
+        DataGenerator generator = new DataGenerator(0, size-1, 123123); //Min, max, seed
+        int dato = generator.next();
+        
+        //Llenamos la queue:
+        for(int i=1; i<size/2; i++) ms.push(generator.next());
+
+        long t = measure(()->{
+            ms.push(dato);
+        });
+        rows.add(row("Stack", "Push", size, t));
+        
+        for(int i=size/2; i<size; i++) ms.push(generator.next());
+        
+        t = measure(()->{
+            bh.consume(ms.pop());
+        });
+        rows.add(row("Stack", "Pop", size, t));
+        
+        t = measure(()->{
+            ms.delete(dato);
+        });
+        rows.add(row("Stack", "Delete", size, t));
+        
+        t = measure(()->{
+            bh.consume(ms.peek());
+        });
+        rows.add(row("Stack", "Peek", size, t));
+        
+        t = measure(()->{
+            bh.consume(ms.size());
+        });
+        rows.add(row("Stack", "Size", size, t));
+        
+        t = measure(()->{
+            bh.consume(ms.isEmpty());
+        });
+        rows.add(row("Stack", "isEmpty", size, t));
+    }
+
     public static void main(String[]args) throws IOException {
         //Diferentes tamaños a probar
-        int []sizes = {1000, 10000, 100000, 1000000, 10000000, 100000000};
+        int []sizes = {10, 50, 75, 100, 500, 750, 1000, 5000, 7500, 10000, 50000, 75000, 100000, 500000, 750000,
+                      1000000, 5000000, 7500000, 10000000, 50000000, 75000000, 100000000}; 
         
         //Esta es la dirección en que se van a guardar los resultados
         String csvPath = "results.csv";
@@ -83,105 +172,46 @@ public class Main{
         //El orden en que lo vamos a hacer es importa. Vamos de estructura en estructura
         for(int size: sizes){
             rows = new ArrayList<>(); //limpiamos arraylist en cada pasada
-
-            DataGenerator generator = new DataGenerator(0, size-1, 323213); //Min, max, seed
-            int[]data = generator.generate(size);
-            //Esto generará un número random para el index del addBefore y el addAfter
-            int ind = generator.next();
-            int dato = generator.next();
             
             //Single Linked List No Tail 
             LinkedList<Integer> sll = new LinkedList<>();
-            
-            testLinkedList (sll, data, ind, dato, rows, size, "SingleLLNoTail");
-            
+            testLinkedList (sll, rows, size, "SingleLLNoTail");
+            sll = null;
+            System.gc();
+
             //Single Linked List With Tail        
             LinkedListWithTail<Integer> sllt = new LinkedListWithTail<>();
-            testLinkedList (sllt, data, ind, dato, rows, size, "SingleLLWithTail");
-               
+            testLinkedList (sllt, rows, size, "SingleLLWithTail");
+            sllt = null;
+            System.gc();
+
             //Doubly Linked List No Tail
             DoublyLinkedNoTail<Integer> dll = new DoublyLinkedNoTail<>();
-            testLinkedList (dll, data, ind, dato, rows, size, "DoublyLLNoTail");
-            
+            testLinkedList (dll, rows, size, "DoublyLLNoTail");
+            dll = null;
+            System.gc();
+
             //Doubly Linked List With Tail
             DoublyLinked<Integer> dllt = new DoublyLinked<>();
-            testLinkedList (dll, data, ind, dato, rows, size, "DoublyLLWithTail");
-            
+            testLinkedList (dllt, rows, size, "DoublyLLWithTail");
+            dllt = null;
+            System.gc();
+
             //MyQueue
             MyQueue<Integer> mq = new MyQueue<>();
-            
-            long t = measure(()->{
-                mq.enqueue(data[0]);
-            });
-            rows.add(row("MyQueue", "Enqueue", size, t));
-            
-            //Llenamos la queue:
-            for(int i=1; i<size; i++) mq.enqueue(data[i]);
-            
-            t = measure(()->{
-               bh.consume(mq.dequeue());
-            });
-            rows.add(row("MyQueue", "Dequeue", size, t));
-            
-            t = measure(()->{
-                mq.delete(data[ind]);
-            });
-            rows.add(row("MyQueue", "Delete", size, t));
-            
-            //Lo dejo como front porque así aparece en el doc guía
-            t = measure(()->{
-                bh.consume(mq.peek());
-            });
-            rows.add(row("MyQueue", "Front", size, t));
-            
-            t = measure(()->{
-                bh.consume(mq.size());
-            });
-            rows.add(row("MyQueue", "Size", size, t));
-            
-            t = measure(()->{
-                bh.consume(mq.isEmpty());
-            });
-            rows.add(row("MyQueue", "isEmpty", size, t));
+            testQueue(mq, rows, size);
+            mq = null;
+            System.gc();
 
             //Stack
             MyStack<Integer> ms = new MyStack<>();
-            
-            t = measure(()->{
-                ms.push(data[0]);
-            });
-            rows.add(row("MyStack", "Push", size, t));
-            
-            //Llenamos la queue:
-            for(int i=1; i<size; i++) ms.push(data[i]);
-            
-            t = measure(()->{
-               bh.consume(ms.pop());
-            });
-            rows.add(row("MyStack", "Pop", size, t));
-            
-            t = measure(()->{
-                ms.delete(data[ind]);
-            });
-            rows.add(row("MyStack", "Delete", size, t));
-            
-            t = measure(()->{
-                bh.consume(ms.peek());
-            });
-            rows.add(row("MyStack", "Peek", size, t));
-            
-            t = measure(()->{
-                bh.consume(ms.size());
-            });
-            rows.add(row("MyStack", "Size", size, t));
-            
-            t = measure(()->{
-                bh.consume(ms.isEmpty());
-            });
-            rows.add(row("MyStack", "isEmpty", size, t));
+            testStack(ms, rows, size);    
+            ms = null;
+            System.gc();
 
             //Exportamos la info que quedó contenido en rows.
             CsvExporter.exportar(rows, csvPath);
+            System.out.printf("Se genero data para, %d\n",size);
         }
     }
     
